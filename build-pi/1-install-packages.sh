f14ed7d1ba074306741c949d6df4c6d4d2de2142
Nicolas Beney <nbeney@gmail.com>
2016-05-02 19:42:01 +0100

    Added build-pi

---
+++ build-pi/1-install-packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+sudo apt-get install dnsutils
+sudo apt-get install emacs24-nox
+sudo apt-get install htop
+sudo apt-get install lsof
+sudo apt-get install nmap
+sudo apt-get install pdns-server pdns-recursor
+sudo apt-get install silversearcher-ag
+sudo apt-get install tmux
