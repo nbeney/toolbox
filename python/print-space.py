14cbb4d14c8c4c2c7d13741a024845ad989dcfd5
Nicolas Beney <nbeney@gmail.com>
2016-05-21 13:07:44 +0100

    Added comments + namedtuple to print-space.py

--- python/print-space.py
+++ python/print-space.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 
+"""
+Command line utility to print out the free space for a list of hosts and directories.
+"""
+
 import subprocess
 
+from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 
 HOSTS = ["pi1", "kano"]
 DIRS = ["/dev", "/", "/dev/shm", "/run", "/run/lock", "/sys/fs/cgroup", "/boot"]
 
+DfStats = namedtuple("DfStats", "total available used used_pct")
+
 def get_space(hosts, dirs, max_workers=None):
+    """
+    Check the free space for a list of hosts and directories.
+
+    Return a dict of the form {host: {dir: DfStats}}.
+
+    <DfStats> is a namedtuple with the following fields: total, available, used, and used_pct.
+    """
+    
     def get_space_for_one_host(host, dirs):
         cmd = ["ssh", host, "df", "-P"] + dirs
         output = subprocess.check_output(cmd).decode('ascii')
         lines = output.split("\n")
-        map_ = {dir_: lines[idx].split()[4] for idx, dir_ in enumerate(dirs, 1)}
+        map_ = {dir_: DfStats(*lines[idx].split()[1:5]) for idx, dir_ in enumerate(dirs, 1)}
         return (host, map_)
 
     max_workers = max_workers or len(hosts)
@@ -22,6 +37,12 @@ def get_space(hosts, dirs, max_workers=None):
         return dict(results)
 
 def print_table(table):
+    """
+    Pretty-print a table.
+
+    <table> is a list of lists. Each row must have the same length.
+    """
+    
     ncols = len(table[0])
     widths = [max(len(row[col]) for row in table) for col in range(ncols)]
     fmt = "  ".join("{{:>{}}}".format(_) for _ in widths)
@@ -31,6 +52,6 @@ def print_table(table):
 if __name__ == "__main__":
     data = get_space(HOSTS, DIRS)
     headers = [[""] + HOSTS]
-    rows = [[dir_] + [data[host][dir_] for host in HOSTS] for dir_ in DIRS]
+    rows = [[dir_] + [data[host][dir_].used_pct for host in HOSTS] for dir_ in DIRS]
     table = headers + rows
     print_table(table)
