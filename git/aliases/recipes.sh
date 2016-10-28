32a4b5aba18c3e420c844f85d58695fd86246572
Nicolas Beney <nbeney@uk.daiwacm.com>
2016-10-28 14:53:16 +0100

    Added git recipes (fixed typo).

--- git/aliases/recipes.sh
+++ git/aliases/recipes.sh
@@ -5,6 +5,6 @@ After cbgit wg checkout <workgroup>, master is sometimes ahead of origin because
 commits from personal-fork got merged in master. If you are sure that all the commits in personal-fork can
 be discarded:
 * Run git log1 --decorate to find what commit origin/master is on -> XXX
-* git reset -hard XXX
+* git reset --hard XXX
 * git push --force personal-fork
 EOF
